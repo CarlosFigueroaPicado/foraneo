44ffbf59f1fed8d23012e81bd40280c9fa831052
CarlosFigueroaPicado <164576695+CarlosFigueroaPicado@users.noreply.github.com>
2025-09-27 10:45:55 -0700

    Add routes and imports for new screens
    Update main.dart to import and define routes for transports, experiences, itineraries, notifications, reviews and payment screens.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/explore_screen.dart';
 import 'screens/detail_screen.dart';
-import 'screens/reservations_screen.dart';
+import 'screens/experiences_screen.dart'
+  import 'screens/reservations_screen.dart';
+import 'screens/transports_screen.dart';
+
+import 'screens/itineraries_screen.dart';
+import 'screens/notifications_screen.dart';
+import 'screens/reviews_screen.dart';
+import 'screens/payment_screen.dart';
+
 import 'screens/profile_screen.dart';
 
 void main() {
@@ -27,6 +35,13 @@ class ForaneoApp extends StatelessWidget {
         '/detail': (context) => const DetailScreen(),
         '/reservations': (context) => const ReservationsScreen(),
         '/profile': (context) => const ProfileScreen(),
+            '/transports': (context) => const TransportsScreen(),
+    '/experiences': (context) => const ExperiencesScreen(),
+    '/itineraries': (context) => const ItinerariesScreen(),
+    '/notifications': (context) => const NotificationsScreen(),
+    '/reviews': (context) => const ReviewsScreen(),
+    '/payments': (context) => const PaymentScreen(),
+
       },
     );
   }
